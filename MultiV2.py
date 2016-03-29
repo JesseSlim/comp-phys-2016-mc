6d7141eb7114edaf4141594c4c62c78f44ccdb61
szwetsloot <sybren1111@gmail.com>
2016-03-29 12:52:45 +0200

    New version

--- MultiV2.py
+++ MultiV2.py
@@ -20,7 +20,7 @@ polymer_size = 2                      # Amount of beads in the polymer
 max_polymer_size = 250                # Maximum polymer size
 polymer_count = 1E5                   # Amount of polymers that we are simulating
 
-T = 5                                 # Temperature in kelvin
+T = 0.1                               # Temperature in kelvin
 epsilon = 0.25                        # Epsilon voor de lennard Jones
 sigma = 0.8                           # Sigma voor de Lennard Jones
 
@@ -29,7 +29,12 @@ theta_weight = np.zeros([theta,1])    # Weight of angles
 theta_weight_sum = 0                  # Sum of the weights
 polymer_weight = 1                    # Weight
 
-multi_step_size = 3                   # Multiprocessor data size communication
+Settings = {
+    'theta': theta,
+    'T': T,
+    'epsilon': epsilon,
+    'sigma': sigma,
+            };
 
 # Create standard Polymer
 BasePolymer = {
@@ -52,28 +57,26 @@ def restart_program():
     """
     os.execv(sys.executable, [sys.executable] + sys.argv)
 
-def workerFunction(q, q2): # This function controls the workers and defines what they should do
+def workerFunction(Settings, q, q2): # This function controls the workers and defines what they should do
     while (1):
-        dicti = q.get();
-        if (dicti == 'Exit'):
-            break;
-        listi = dicti['List']
-        
-        try:
-            len(listi)
-        except:
-            print("ListError - Self");
-            continue;
-            
-        if (dicti['Task'] == 'addBead'):
-            for i in range(0,len(listi)-1):
-                listi[i] = addBead(listi[i]);
-            q2.put(dicti);
-        time.sleep(0.001);
+        if (not q.empty()):
+            dicti = q.get();
+            if (dicti == 'Exit'):
+                break;
+
+            if (dicti['Task'] == 'addBead'):
+                dicti['Polymer'] = addBead(Settings, dicti['Polymer']);
+                q2.put(dicti);
+        else:
+            time.sleep(0.001);
 
 @jit
-def addBead(var): # This function adds a bead to a polymer
+def addBead(Settings, var): # This function adds a bead to a polymer
     # We start by checking the energy and thus the change of all theta directions
+    theta = Settings['theta'];
+    signma = Settings['sigma'];
+    T = Settings['T'];
+    epsilon = Settings['epsilon'];
     theta_weight_sum = 0
     while (theta_weight_sum == 0):
         theta_offset = random.uniform(0,1)
@@ -108,122 +111,126 @@ def addBead(var): # This function adds a bead to a polymer
     if (var['Size'] == 3):
         var['Weight3'] = var['Weight']
     return var
-
+    
+def main(Settings,q,q2,BasePolymer,max_polymer_size): # Wrapper for main to secure local data
+    polymer_size = 2;
+    upLimInput = int(sys.argv[1]);
+    print("Start");
+    start_time = time.time();
+    
+    # Create a subfolder in Data were we are going to save everything
+    i = 1;
+    while (os.path.isfile(str(os.getcwd())+"/Data/Survivors_"+str(sys.argv[2])+"_"+str(i)+".npy")):
+        i+=1;
+    file = str(os.getcwd())+"/Data/Survivors_"+str(sys.argv[2])+"_"+str(i);
+    
+    # Create a list of all polymers
+    polarray = []
+    
+    # Fill the list with N polymers
+    N = 100
+    for i in range(0,N):
+        polarray.append(BasePolymer)
+    
+    # Now we start the actual process
+    steptime = 0;
+    polymer_start_size = polymer_size
+    while (polymer_size < max_polymer_size):
+        if (polymer_size > polymer_start_size):
+            print("Iteration took: "+str((time.time() - steptime))+"\t\t Speed = "+(str(Amount / (time.time() - steptime))));
+        steptime = time.time();
+        if msvcrt.kbhit():
+            if (msvcrt.getch() == 'q'):
+                break
+            print ("Key pressed: "+ str(msvcrt.getch()))
+               
+        # Add a bead to every polymer. We send this task to the workers.
+        c = 0;
+        jobCount = 0
+           
+        # Submit the tasks to the workers
+        while(c < len(polarray)):
+            jobCount = jobCount + 1
+            q.put({'Polymer':polarray[c], 'Id':c, 'Task':'addBead'})
+            c = c + 1;
+               
+               
+        # Retrieve the tasks from the workers
+        while (jobCount > 0):
+            if (not q2.empty()):
+                dicti = q2.get();
+                polarray[dicti['Id']] = dicti['Polymer']
+                jobCount -= 1;
+            else:
+                time.sleep(0.001);
+    
+        # Next we need to prune the polymers
+        #We do this in the main thread because it shouldn't take much time and saves overhead
+        TotWeight = 0;
+        Amount = len(polarray);
+        for i in range(0,Amount):
+            TotWeight += polarray[i]['Weight'];
+        AvgWeight = TotWeight / Amount
+        print("AVgWeight = "+str(AvgWeight)+" Amomunt = "+str(Amount))
+        upLim = 2 ** (math.log(Amount,10)-upLimInput)
+        lowLim = 1.5 ** (math.log(Amount,10)-1)
+        for i in range(0,Amount):
+            if (polarray[i]['Weight'] > upLim * AvgWeight / polarray[i]['Weight3']): # Branch
+                polarray[i]['Weight'] /= 2;
+                polarray.append(deepcopy(polarray[i]))
+            if (polarray[i]['Weight'] < lowLim * AvgWeight / polarray[i]['Weight3']): # Prune
+                rand = random.uniform(0,1)
+                if (rand < 0.5):
+                    polarray[i]['Weight'] *= 2
+                else:
+                    polarray[i]['Alive'] = False;
+    
+        # Now we don't want the dead polymers to crowd the list,
+        # so we are going to remove them and save them in a file.
+        i = Amount;
+        dead = 0;
+        while (i > 0):
+            i -= 1;
+            if (polarray[i]['Alive'] == False):
+                dead += 1;
+                polarray.pop(i)
+        # Save the dead polymers to a file
+        polymer_size += 1;
+        print("PolymerSize = "+str(polymer_size)+" Living = "+str(len(polarray))+" Just died = "+str(dead));
+    print("Exiting");
+    
+    # Now we have to save the polymers to a data file
+    np.savez(file, Polymers=polarray, Settings=Settings);
+    # Clear some memory
+    while (len(polarray) > 0):
+        polarray.pop(len(polarray)-1);
+    print("Finished in: "+str(time.time() - start_time));
+    print("So now we are done. Congratulations!");
+    time.sleep(0.5);
+    print(""); time.sleep(0.5);
+    print(""); time.sleep(0.5);
+    print(""); time.sleep(0.5);
+    print(""); time.sleep(0.5);
+    print(""); time.sleep(0.5);
+    print(""); time.sleep(0.5);
+    print(""); time.sleep(0.5);
+    print("Just kidding");
+    print(""); time.sleep(0.5);
+    print("Restarting!");
 
 # Master function - This function initializes everything and starts the workers
 if __name__ == '__main__':
-    # Create the workers
-    cpuList = []
-    for i in range(0,cpu_count()): #Cpu count utilizes your computer. use cpu_count()-1 to do other things.
-        cpuList.append(Process(target=workerFunction, args=(q,q2,)))
-        cpuList[i].start();
-    while(1):
-        polymer_size = 2;
-        upLimInput = int(sys.argv[1]);
-        print("Start");
-        start_time = time.time();
+    while(1): # Since we can never have enough data, we are now restarting the programm and running it again
+        # Create the workers
+        cpuList = []
+        for i in range(0,cpu_count()): #Cpu count utilizes your computer. use cpu_count()-1 to do other things.
+            cpuList.append(Process(target=workerFunction, args=(Settings,q,q2,)))
+            cpuList[i].start();
+        main(Settings, q,q2,BasePolymer,max_polymer_size);
+        for i in range(0,100): # Spam the exit message to the queue to make sure all workers finish
+            q.put('Exit');
+        for i in range(0, len(cpuList)):
+            cpuList[i].join()
+        Settings['T'] = Settings['T'] + 0.1;
 
-        # Create a subfolder in Data were we are going to save everything
-        i = 1;
-        while (os.path.isdir(str(os.getcwd())+"/Data/"+str(sys.argv[2])+"_"+str(i))):
-            i+=1;
-        os.makedirs(str(os.getcwd())+"/Data/"+str(sys.argv[2])+"_"+str(i))
-        folder = str(os.getcwd())+"/Data/"+str(sys.argv[2])+"_"+str(i)+"/";
-        print("Folder = "+str(folder));
-
-        # Create a list of all polymers
-        polarray = []
-
-        # Fill the list with N polymers
-        N = 2
-        for i in range(0,966):
-            polarray.append(BasePolymer)
-
-        # Now we start the actual process
-        steptime = 0;
-        polymer_start_size = polymer_size
-        while (polymer_size < max_polymer_size):
-            if (polymer_size > polymer_start_size):
-                print("Iteration took: "+str((time.time() - steptime))+"\t\t Speed = "+(str(Amount / (time.time() - steptime))));
-            steptime = time.time();
-            if msvcrt.kbhit():
-                if (msvcrt.getch() == 'q'):
-                    break
-                print ("Key pressed: "+ str(msvcrt.getch()))
-            # Add a bead to every polymer. We send this task to the workers.
-            c = 0;
-            jobCount = 0;
-            # Submit the tasks to the workers
-            while( c < len(polarray)):
-                if (len(polarray) - c < 0):
-                    q.put({'List':polarray[c:], 'Id':c, 'Task':'addBead'})
-                else:
-                    q.put({'List':polarray[c:c+multi_step_size], 'Id':c, 'Task':'addBead'})
-                jobCount += 1;
-                c = c + multi_step_size;
-            # Retrieve the tasks from the workers
-            while (jobCount > 0):
-                dicti = q2.get();
-                if (len(polarray)-dicti['Id'] < 0):
-                    polarray[dicti['Id']:] = dicti['List']
-                else:
-                        polarray[dicti['Id']:dicti['Id']+multi_step_size] = dicti['List']
-                jobCount -= 1;
-
-            # Next we need to prune the polymers
-            # We do this in the main thread because it shouldn't take much time and saves overhead
-            TotWeight = 0;
-            Amount = len(polarray);
-            for i in range(0,Amount):
-                TotWeight += polarray[i]['Weight'];
-            AvgWeight = TotWeight / Amount
-            print("AVgWeight = "+str(AvgWeight)+" Amomunt = "+str(Amount))
-            upLim = 2 ** (math.log(Amount,10)-upLimInput)
-            lowLim = 1.5 ** (math.log(Amount,10)-1)
-            for i in range(0,Amount):
-                if (polarray[i]['Weight'] > upLim * AvgWeight / polarray[i]['Weight3']): # Branch
-                    polarray[i]['Weight'] /= 2;
-                    polarray.append(deepcopy(polarray[i]))
-                if (polarray[i]['Weight'] < lowLim * AvgWeight / polarray[i]['Weight3']): # Prune
-                    rand = random.uniform(0,1)
-                    if (rand < 0.5):
-                        polarray[i]['Weight'] *= 2
-                    else:
-                        polarray[i]['Alive'] = False;
-
-            # Now we don't want the dead polymers to crowd the list,
-            # so we are going to remove them and save them in a file.
-            DeadList = [];
-            i = Amount;
-            while (i > 0):
-                i -= 1;
-                if (polarray[i]['Alive'] == False):
-                    DeadList.append(polarray.pop(i))
-            # Save the dead polymers to a file
-            np.save(folder+"/Dead_"+str(polymer_size), DeadList);
-            polymer_size += 1;
-            print("PolymerSize = "+str(polymer_size)+" Living = "+str(len(polarray))+" Just died = "+str(len(DeadList)));
-        print("Exiting");
-
-        # Now we have to save the polymers to a data file
-        np.save(folder+'/Survivors', polarray);
-        print("Finished in: "+str(time.time() - start_time));
-        print("So now we are done. Congratulations!");
-        time.sleep(0.5);
-        print(""); time.sleep(0.5);
-        print(""); time.sleep(0.5);
-        print(""); time.sleep(0.5);
-        print(""); time.sleep(0.5);
-        print(""); time.sleep(0.5);
-        print(""); time.sleep(0.5);
-        print(""); time.sleep(0.5);
-        print("Just kidding");
-        print(""); time.sleep(0.5);
-        print("Restarting!");
-        # Since we can never have enough data, we are now restarting the programm and running it again
-    for i in range(0,100): # Spam the exit message to the queue to make sure all workers finish
-        q.put('Exit');
-
-        # Stop the workers when their work is done - Good job guys
-    for i in range(0, len(cpuList)):
-        cpuList[i].join()
+            # Stop the workers when their work is done - Good job guys
